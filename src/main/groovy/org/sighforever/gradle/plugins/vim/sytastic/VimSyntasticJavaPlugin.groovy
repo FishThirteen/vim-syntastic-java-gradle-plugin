d425cd60f5b0c4955dc56355a53f5901fcce1f82
Sighforever <sighforever@gmail.com>
2014-02-14 18:18:46 +0800

    complete main function for vim syntastic java plug in

---
+++ src/main/groovy/org/sighforever/gradle/plugins/vim/sytastic/VimSyntasticJavaPlugin.groovy
@@ -0,0 +1,38 @@
+package org.sighforever.gradle.plugins.vim.syntastic
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class VimSyntasticJavaPlugin implements Plugin<Project> {
+  void apply(Project project) {
+    project.extensions.create("syntastic", SyntasticData)
+
+    def classPathCollection = new HashSet<String>()
+
+    def addPath = { proj ->
+      if (proj.plugins.hasPlugin("java")) {
+        proj.configurations.compile.each { jar ->
+            classPathCollection.add(jar);
+          }
+        }
+
+      proj.sourceSets.each { srcSet ->
+        srcSet.java.srcDirs.each { dir ->
+          classPathCollection.add(dir.absolutePath)
+        }
+      }
+    }
+
+    project.task("SyntasticJava") << {
+      project.allprojects.each { proj -> 
+        addPath(proj) 
+      }
+
+      new File(project.projectDir.absolutePath + "/" + "${project.syntastic.classpathFile}").text = classPathCollection.collect().join("\n")
+    }
+  }
+} 
+
+class SyntasticData {
+  def String classpathFile = ".syntastic-classpath"
+}
